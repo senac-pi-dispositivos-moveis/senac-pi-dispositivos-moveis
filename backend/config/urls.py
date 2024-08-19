6270e7f04a5dfdb5b16f2ea0b881190552d77f3b
Gustavo Sousa <gustavo@gustavosousa.me>
2024-08-19 20:18:36 -0300

    Adding authentication process

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
+from rest_framework.authtoken import views as rest_views
 
 from core.views import SpecialistViewSet
 
@@ -10,4 +11,5 @@ router.register('specialists', SpecialistViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
+    path('api/api-token-auth/', rest_views.obtain_auth_token),
 ]
