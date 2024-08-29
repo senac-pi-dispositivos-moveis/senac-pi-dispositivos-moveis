fa60ec7134b5874e95c13d4d728d9fc7043e9ae8
KeillaLima <58301280+KeillaLima@users.noreply.github.com>
2024-08-29 15:50:03 -0300

    Update script.js

--- Teste Autismo Responsivo 2/src/javascript/script.js
+++ Teste Autismo Responsivo 2/src/javascript/script.js
@@ -63,4 +63,9 @@ $(document).ready(function() {
         duration: 2000,
         distance: "20%"
     });
+    ScrollReveal().reveal("#clinicas", {
+        origin: "left", 
+        duration: 2000,
+        distance: "20%"
+    });
 });
